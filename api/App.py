9b04b30bd64734fe7f07b93f72575ec89b7182c5
Dylan Mooers <dmooers@calpoly.edu>
2021-05-04 11:57:01 -0700

    Item Page communicated w backend

--- api/App.py
+++ api/App.py
@@ -14,6 +14,17 @@ listings = [
         "https://target.scene7.com/is/image/Target/GUEST_9251c93b-9ab1-42d4-beed-5f2ea738a131?fmt=webp&wid=1400&qlt=80",
         "id": "asdbcs",
     },
+    {
+        "title": "Never Opened Before PS5",
+        "price": 849.99,
+        "desc":
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        "lngLat": [-121.4, 45.38],
+        "imgUrl":
+        "https://gmedia.playstation.com/is/image/SIEPDC/playstation-5-with-dualsense-front-product-shot-01-ps5-en-30jul20?$1600px--t$",
+        "id": "asdkj12esa",
+    },
+
 ]
 
 
@@ -24,11 +35,11 @@ def get_items():
 
 
 @app.route('/items/<id>')
-def get_item():
-    if request.method == 'GET':
+def get_item(id):
+    if request.method == 'GET' and id:
         item = None
         for itm in listings:
-            if itm.id == id:
+            if itm['id'] == id:
                 item = itm
         if item == None:
             return jsonify({"error": "Item not found"}), 404
