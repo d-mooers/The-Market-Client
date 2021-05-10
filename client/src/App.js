0b10a71e4a1a8ce5f4719de7ff084deb18872fd4
Dylan Mooers <dmooers@calpoly.edu>
2021-05-10 12:20:51 -0700

    Tested to make sure context works

--- client/src/App.js
+++ client/src/App.js
@@ -14,24 +14,34 @@ import { UserProvider } from "./UserContext";
 
 const RoutedHeader = withRouter(Header);
 
-const [user, setUser] = useState({});
+const App = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    _id: "",
+    loggedIn: false,
+  });
+  //const user = "hello";
+  //const setUser = () => null;
 
-const App = () => (
-  <UserProivder value={{ user, setUser }}>
-    <RoutedHeader />
-    <Suspense fallback={HomePage}>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/browse" exact component={ViewItemsPage} />
-      <Route path="/profile" exact component={ViewProfilePage} />
-      <Route path="/item" component={ItemPage} />
-      <Route path="/login" exact component={ViewLoginPage} />
-      <Route path="/sell" exact component={ListItemPage} />
-      <Route path="/questions" component={Questions} />
-      <Route path="/register" exact component={Register} />
-      <Route path="/checkout" component={Checkout} />
-      {/*Add new routes here!*/}
-    </Suspense>
-  </UserProivder>
-);
+  return (
+    <UserProvider value={{ user, setUser }}>
+      <RoutedHeader />
+      <Suspense fallback={HomePage}>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/browse" exact component={ViewItemsPage} />
+        <Route path="/profile" exact component={ViewProfilePage} />
+        <Route path="/item" component={ItemPage} />
+        <Route path="/login" exact component={ViewLoginPage} />
+        <Route path="/sell" exact component={ListItemPage} />
+        <Route path="/questions" component={Questions} />
+        <Route path="/register" exact component={Register} />
+        <Route path="/checkout" component={Checkout} />
+        {/*Add new routes here!*/}
+      </Suspense>
+    </UserProvider>
+  );
+};
 
 export default App;
