724da21f1e93486f6e7c180e011d81d48fb0d173
Dylan Mooers <dmooers@calpoly.edu>
2021-05-13 15:42:52 -0700

    added route to DELETE item

--- api/App.py
+++ api/App.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from Utils import verifyListingShape, verifyUserShape, verifyUser, verifyLoginShape
+from Utils import verifyListingShape, verifyUserShape, verifyUser, verifyLoginShape, verifyAuthShape
 from Models import Listings, User
 app = Flask(__name__)
 CORS(app)
@@ -21,7 +21,7 @@ def get_items():
         return jsonify(listing), 201
 
 
-@app.route('/items/<id>')
+@app.route('/items/<id>', methods=['GET', 'DELETE'])
 def get_item(id):
     if request.method == 'GET' and id:
         item = Listings({'_id': id})
@@ -29,6 +29,23 @@ def get_item(id):
             return jsonify(item), 200
         if item == None:
             return jsonify({"error": "Item not found"}), 404
+    if request.method == 'GET' and id:
+        auth = request.get_json()
+        item = Listings({'_id': id})
+
+        missingFields = verifyAuthShape(auth)
+        if len(missingFields) > 0:
+            return jsonify({"message": "Bad request, missing fields",
+                            "details": missingFields}), 400
+        if not item.reload():
+            return jsonify({"error": "Item not found"}), 404
+        if not verifyUser(auth['authId'], auth['_id']) or auth['_id'] != item['owner']:
+            return jsonify({'erorr': 'Unauthorized'}), 401
+
+        resp = item.remove()
+        if resp.deleted_count == 1:
+            return jsonify({}), 204
+        return jsonify({"error": "Item not found"}), 404
 
 
 @app.route('/users', methods=['GET', 'POST'])
