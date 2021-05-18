5e0d8946ce8a74266d7027e34ab714fc736afcd1
Dylan Mooers <dmooers@calpoly.edu>
2021-05-17 22:36:03 -0700

    Added some fun styling

--- client/src/components/ViewItems/ItemList.js
+++ client/src/components/ViewItems/ItemList.js
@@ -6,13 +6,11 @@ const ItemList = (props) => {
   return (
     <Grid container wrap="wrap" spacing={2}>
       {props.items.map((itm, i) => (
-        <Grid item xl={3} lg={3} md={4} sm={5}>
-          <Item
-            {...itm}
-            key={`itm-${i}`}
-            handleClick={() => props.goToItem(itm._id)}
-          />
-        </Grid>
+        <Item
+          {...itm}
+          key={`itm-${i}`}
+          handleClick={() => props.goToItem(itm._id)}
+        />
       ))}
     </Grid>
   );
