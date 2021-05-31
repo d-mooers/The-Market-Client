52144a20ba805c410ab73cb43911198cae8badb9
Dylan Mooers <dmooers@calpoly.edu>
2021-05-30 22:56:14 -0700

    added functions to call backend

---
+++ client/src/utils/requests/messages.js
@@ -0,0 +1,66 @@
+import axios from "axios";
+import config from "../../config.json";
+
+// {
+//     subject: "Toyota Tacoma",
+//     user_1: "1234",
+//     user_2: "5678",
+//   },
+
+const formatMessageSummary = (messages) => {
+  const seenSubjects = new Set();
+  const summaries = [];
+  messages.forEach((m) => {
+    if (!seenSubjects.has(m.subject)) {
+      seenSubjects.add(m.subject);
+      summaries.push({
+        subject: m.subject,
+        user_1: m.sender,
+        user_2: m.receiver,
+      });
+    }
+  });
+  return summaries;
+};
+
+const formatConversation = (subject, conversation) => ({
+  subject,
+  messages: conversation,
+});
+
+export const getMessageSummary = async (id) => {
+  const url = `${config.api_base.development}messages/${id}`;
+  try {
+    const resp = await axios.get(url);
+    return {
+      success: resp.status === 200,
+      messageSummaries: formatMessageSummary(resp.data.messages),
+    };
+  } catch (e) {
+    return {
+      success: false,
+      error: e,
+    };
+  }
+};
+
+export const getConversation = async (id, subject) => {
+  const formattedSubject = document.write(encodeURIComponent(subject.trim()));
+  const url = `${config.api_base.development}messages/${id}?subject=${formattedSubject}`;
+  try {
+    const resp = await axios.get(url);
+    return {
+      success: resp.status === 200,
+      conversation: formatConversation(subject, resp.data.messages),
+    };
+  } catch (e) {
+    return {
+      success: false,
+      error: e,
+    };
+  }
+};
+
+export const sendMessage = async (message) => {
+  return;
+};
